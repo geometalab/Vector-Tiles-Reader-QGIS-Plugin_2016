28a6f75691a2334f948b620433b92c8ca435d0f8
Maroben <dijandiwei@daishen.ch>
2016-04-13 13:24:14 +0200

    refactored some functions

--- vtr_model.py
+++ vtr_model.py
@@ -25,8 +25,6 @@ import sqlite3
 import gzip
 import os
 import uuid
-import Canvas
-import StringIO
 
 extent = 4096
 geo = []  # 0: zoom, 1: easting, 2: northing
@@ -54,30 +52,14 @@ class Model:
         self._iface = iface
         self.temporary_layer = []
         self.database_source = database_source
-        # self._init_connections()
-
-    # def _init_connections(self):
-    #     vector_layer = QgsVectorLayer()
-    #     vector_layer.geometryChanged.connect(self._refresh)
-    #     vector_layer.screenUpdateRequested.connect(self._refresh)
-    #     QgsMapCanvas().scaleChanged.connect(self._refresh)
-    #
-    # def _refresh(self):
-    #     scale = self.canvas.scale()
-    #     coordinates = self.canvas.xyCoordinates()
-    #     with open(self._tmp2, 'w') as f:
-    #         f.write(scale)
-    #         f.write(coordinates)
-    #     print "refresh"
-    #     # self.mbtiles(scale=scale, coordinates=coordinates)
 
     def mbtiles(self, scale=None, coordinates=None):
         # connect to a mb_tile file and extract the data
-        con = sqlite3.connect(self.database_source)
-        c = con.cursor()
-        c.execute("SELECT * FROM tiles WHERE zoom_level = 12;")
+        cursor = self.database_cursor
+        command = self.database_command
+        data = cursor.execute(command)
 
-        for index, row in enumerate(c):
+        for index, row in enumerate(data):
             self._geo = [row[0], row[1], row[2]]
 
             with open(self._tmp, 'wb') as f:
@@ -104,75 +86,75 @@ class Model:
         for name in decoded_data:
             for index, value in enumerate(decoded_data[name]['features']):
                 self.geojson_data["features"].append(
-                    build_object(decoded_data[name]["features"][index], geometry)
+                    self.build_object(decoded_data[name]["features"][index], geometry)
                 )
         return self.geojson_data
 
     def load_layer(self, json_src):
+        # load the created geojson into qgis
         self._iface.addVectorLayer(json_src, "a name", "ogr")
+
+    @property
+    def database_cursor(self):
+        # connect to the database and return its corresponding cursor
+        con = sqlite3.connect(self.database_source)
+        return con.cursor()
+
+    @property
+    def database_command(self):
+        # create a suitable sql query (TODO)
+        return "SELECT * FROM tiles WHERE zoom_level = 12 LIMIT 5;"
 
     @property
     def unique_file_name(self):
         unique_name = uuid.uuid4()
         return "%s/data/tmp/%s.geojson" % (self.directory, unique_name)
 
+    def build_object(self, data, geometry):
+        #  single feature structure
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": self.geometry_type(data),
+                "coordinates": self.mercator_geometry(data["geometry"], data["type"], geometry)
+            },
+            "properties": data["properties"]
+        }
+        return feature
 
-# def zoom_level():
-#     scale = QgsMapCanvas().scale()
-#     scale_index = [500000000, 250000000, 150000000, 70000000, 35000000, 15000000, 10000000,
-#                    4000000, 2000000, 1000000, 500000, 250000, 150000, 70000, 35000]
-#     index = 0
-#     while index < len(scale_index) and scale < scale_index[index]:
-#         index += 1
-#     return index
+    def mercator_geometry(self, coordinates, geo_type, geometry):
+        # recursively iterate through all the points and create an array,
+        # if it is just a point remove the outer barckets.
+        tmp = []
+        for index, value in enumerate(coordinates):
+            if isinstance(coordinates[index][0], int):
+                tmp.append(self.calculate_geometry(coordinates[index], geometry))
+            else:
+                tmp.append(self.mercator_geometry(coordinates[index], 0, geometry))
+        if geo_type == 1:
+            return tmp[0]
+        return tmp
 
+    @staticmethod
+    def geometry_type(data):
+        # get the feature type
+        options = {
+            1: "Point",
+            2: "LineString",
+            3: "Polygon",
+            4: "MultiPoint",
+            5: "MultiLineString",
+            6: "MultiPolygon"
+        }
+        return options[data["type"]]
 
-def build_object(data, geometry):
-    #  single feature structure
-    feature = {
-        "type": "Feature",
-        "geometry": {
-            "type": geometry_type(data),
-            "coordinates": mercator_geometry(data["geometry"], data["type"], geometry)
-        },
-        "properties": data["properties"]
-    }
-    return feature
-
-
-def geometry_type(data):
-    # get the feature type
-    options = {
-        1: "Point",
-        2: "LineString",
-        3: "Polygon",
-        4: "MultiPoint",
-        5: "MultiLineString",
-        6: "MultiPolygon"
-    }
-    return options[data["type"]]
-
-
-def mercator_geometry(coordinates, geo_type, geometry):
-    # recursively iterate through all the points and create an array,
-    # if it is just a point remove the outer barckets.
-    tmp = []
-    for index, value in enumerate(coordinates):
-        if isinstance(coordinates[index][0], int):
-            tmp.append(calculate_geometry(coordinates[index], geometry))
-        else:
-            tmp.append(mercator_geometry(coordinates[index], 0, geometry))
-    if geo_type == 1:
-        return tmp[0]
-    return tmp
-
-
-def calculate_geometry(coordinates, geometry):
-    # calculate the mercator geometry using external library
-    # geometry:: 0: zoom, 1: easting, 2: northing
-    tmp = GlobalMercator().TileBounds(geometry[1], geometry[2], geometry[0])
-    delta_x = tmp[2] - tmp[0]
-    delta_y = tmp[3] - tmp[1]
-    merc_easting = int(tmp[0] + delta_x / extent * coordinates[0])
-    merc_northing = int(tmp[1] + delta_y / extent * coordinates[1])
-    return [merc_easting, merc_northing]
+    @staticmethod
+    def calculate_geometry(coordinates, geometry):
+        # calculate the mercator geometry using external library
+        # geometry:: 0: zoom, 1: easting, 2: northing
+        tmp = GlobalMercator().TileBounds(geometry[1], geometry[2], geometry[0])
+        delta_x = tmp[2] - tmp[0]
+        delta_y = tmp[3] - tmp[1]
+        merc_easting = int(tmp[0] + delta_x / extent * coordinates[0])
+        merc_northing = int(tmp[1] + delta_y / extent * coordinates[1])
+        return [merc_easting, merc_northing]
